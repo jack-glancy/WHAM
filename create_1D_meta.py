d0343c957f329620e3363207d49c92ef1184c662
Jack Glancy <83410114+jack-glancy@users.noreply.github.com>
2021-07-13 14:16:15 +0100

    Create create_1D_meta.py

---
+++ create_1D_meta.py
@@ -0,0 +1,19 @@
+import sys
+
+# define inital x coord value
+
+x = 1.48
+xinc = 0.1
+nwin = 17
+
+# define write line
+def main():
+    with open('meta.dat', 'w+') as f:
+        for i in range(nwin):
+                write_line(f, i)
+
+def write_line(inp_file, i):
+    xval = x + xinc * i
+    inp_file.write('diag_%d.dat %0.2f 400 310\n' %(i, xval))
+
+main()
